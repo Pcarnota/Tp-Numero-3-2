3f14bc565df8dcbb486906d9da4a7e3165ba08fa
Pcarnota <pablocarnotacai@gmail.com>
2026-09-11 14:36:52 -0300

    Inicializamos el proyecto del Ejercicio 3.2

---
+++ src/tpnumero3/pkg2/TPNumero32.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpnumero3.pkg2;
+
+/**
+ *
+ * @author Pablo Carnota
+ */
+public class TPNumero32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
